5ddab1e7d004f9931a7e32bb6f337c3c7a27125d
gabsuns <gabrielparedes8434@gmail.com>
2023-07-15 09:32:08 -0400

    finish table_task

---
+++ image.php
@@ -0,0 +1,14 @@
+<?php
+    include("database/db.php");
+
+    $id= $_GET['id'];
+
+    $query= "SELECT image FROM task where id=$id";
+    $result_task = mysqli_query($conn, $query);
+
+    $img_data= mysqli_fetch_assoc($result_task);
+    header("Content-type: image/jpg");
+
+    echo $img_data['image'];
+?>
+
